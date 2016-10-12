e46d9dbb0c85e06fa8ce262b8a1274bcff1755f7
katieee101010 <kstardog@gmail.com>
2016-10-11 18:00:13 -0700

    unicorn

---
+++ main.js
@@ -0,0 +1,43 @@
+// We create our only state
+var mainState = {
+    // Here we add all the functions we need for our state
+    // For this project we will just have 3
+    preload: function () {
+        // This function will be executed at the beginning
+        // That's where we load the game's assets
+        // Load the image
+        game.load.image("unicorn","unicorn.jpg");
+    }
+    , create: function () {
+        // This function is called after the 'preload' function 
+        // Here we set up the game, display sprites, etc.
+        this.keyboard = game.input.keyboard.createCursorKeys();
+        this.player = game.add.sprite(300,300,"unicorn")
+        this.player.scale.setTo(1,1);
+       
+        game.physics.arcade.enable(this.player);
+        this.player.body.gravity.y = 300;
+        this.player.body.collideWorldBounds = true;
+    }
+    , update: function () {
+        this.player.body.velocity.x = 0;
+        
+        if(this.keyboard.right.isDown){
+            
+            this.player.body.velocity.x = 500;
+        }else if(this.keyboard.left.isDown) {
+            this.player.body.velocity.x = -500;
+             }
+        
+          if(this.keyboard.up.isDown){
+              this.player.body.velocity.y = -500;
+          }
+       
+        // This contains Game Logic 
+    }
+};
+// We initialize Phaser
+var game = new Phaser.Game(800, 800, Phaser.AUTO, '');
+// And we tell Phaser to add and start our 'main' state
+game.state.add('main', mainState);
+game.state.start('main');
